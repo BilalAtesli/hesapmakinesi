47e98c930d47f701e53a8067fb99761d1f6aaf2a
Bilal Atesli <ateslibilal@gmail.com>
2023-12-17 18:38:49 +0300

    toplama ve sifirlama fonksiyonu oluşturuldu button click olayine eklendi

--- script.js
+++ script.js
@@ -10,6 +10,31 @@ const sonucDOM=document.getElementById("sonuc");
 
 const btnToplaDOM=document.getElementById("topla");
 
+btnToplaDOM.addEventListener("click",topla);
+
+// Toplama
+function topla()
+{
+	if(sayi1DOM.value=="" || sayi2DOM.value=="")
+	{
+		alert("Bos alan bırakmayınız")
+	}
+	else
+	{
+		sayi1Value=parseInt(sayi1DOM.value);
+		sayi2Value=parseInt(sayi2DOM.value);
+		sonuc=sayi1Value+sayi2Value;
+		sonucDOM.value=sonuc;
+	}	
+	sifirla();
+}
+function sifirla()
+{
+	sayi1Value="";
+	sayi2Value="";
+	sayi1DOM.value="";
+	sayi2DOM.value="";
+}
 
 
 
