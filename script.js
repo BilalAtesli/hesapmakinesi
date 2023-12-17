9bcea5c7e9313fd1290bab16e32e5b796ccb4954
Bilal Atesli <ateslibilal@gmail.com>
2023-12-17 18:36:42 +0300

    degiskenler olusturuldu elementlere ulasildi

---
+++ script.js
@@ -0,0 +1,17 @@
+let sayi1Value;
+let sayi2Value;
+let sonuc;
+
+const sayi1DOM=document.getElementById("sayi-1");
+
+const sayi2DOM=document.getElementById("sayi-2");
+
+const sonucDOM=document.getElementById("sonuc");
+
+const btnToplaDOM=document.getElementById("topla");
+
+
+
+
+
+
